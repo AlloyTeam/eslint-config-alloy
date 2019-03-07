b6dc7359087846f88c2d87f0d8c5a1e5fb1f4319
DiamondYuan <admin@diamondyuan.com>
2019-03-07 17:08:33 +0800

    test: 修改测试方式

--- test/index.js
+++ test/index.js
@@ -1,29 +1,77 @@
 const assert = require('assert');
 const eslint = require('eslint');
+const path = require('path');
 
 const CLIEngine = eslint.CLIEngine;
-const cli = new CLIEngine();
-
-const goodReport = cli.executeOnFiles([
-    './**/good.js',
-    './**/good.vue',
-    './**/good.ts',
-    './**/good.tsx'
-]);
-
-goodReport.results.forEach((goodReportForOneFile) => {
-    assert.equal(goodReportForOneFile.errorCount, 0, `${goodReportForOneFile.filePath} should have no error`);
+const cli = new CLIEngine({
+    ignore: false
 });
 
-const badReport = cli.executeOnFiles([
-    './**/bad.js',
-    './**/bad.vue',
-    './**/bad.ts',
-    './**/bad.tsx'
-]);
+const ruleList = [
+    {
+        root: 'test/index',
+        rulePrefix: '',
+        ignore: ['no-dupe-args']
+    },
+    {
+        root: 'test/react',
+        rulePrefix: 'react/'
+    },
+    {
+        root: 'test/typescript',
+        rulePrefix: 'typescript/',
+        fileExtension: ['ts']
+    },
+    {
+        root: 'test/typescript-react',
+        rulePrefix: 'react/',
+        fileExtension: ['tsx']
+    },
+    {
+        root: 'test/vue',
+        rulePrefix: 'vue/',
+        fileExtension: ['vue']
+    }
+];
 
-badReport.results.forEach((badReportForOneFile) => {
-    assert(badReportForOneFile.errorCount > 0, `${badReportForOneFile.filePath} should have at least one error`);
+ruleList.forEach(rule => {
+    const { root, rulePrefix, fileExtension = ['js'], ignore = [] } = rule;
+
+    const goodReport = cli.executeOnFiles(
+        fileExtension.map(extension => `${root}/**/*/good.${extension}`)
+    );
+    goodReport.results.forEach(goodReportForOneFile => {
+        const { errorCount, filePath } = goodReportForOneFile;
+        assert.equal(errorCount, 0, `${filePath} should have no error`);
+    });
+
+    const badReport = cli.executeOnFiles(
+        fileExtension.map(extension => `${root}/**/*/bad.${extension}`)
+    );
+    badReport.results.forEach(badReportForOneFile => {
+        const { filePath, messages } = badReportForOneFile;
+
+        const ruleName = path
+            .dirname(filePath)
+            .split(path.sep)
+            .pop();
+
+        const errorCount = messages.filter(
+            o => o.ruleId === `${rulePrefix}${ruleName}`
+        ).length;
+
+        if (ignore.includes(ruleName)) {
+            return;
+        }
+
+        if (errorCount === 0) {
+            console.log(messages);
+        }
+        assert(
+            errorCount > 0,
+            `${filePath} should have error of rule ${ruleName}`
+        );
+    });
+
+    console.log(`all test pass in ${root}`);
 });
-
-console.log('Test passed!');
