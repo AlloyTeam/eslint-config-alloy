81954453e967bcdcaf972bfea343d8abbcc4acb3
xcatliu <xcatliu@gmail.com>
2019-03-23 21:34:10 +0800

    补充三个规则

---
+++ test/index/multiline-comment-style/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    rules: {
+        /**
+         * 约束多行注释的格式
+         * @category Stylistic Issues
+         * @reason 能写注释已经不容易了，不需要限制太多
+         * @fixable
+         */
+        'multiline-comment-style': 'off'
+    }
+};
